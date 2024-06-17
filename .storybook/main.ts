225ee59e7ab7dd9353afc103cbe025e0b4883367
reckyy <srreki98@gmail.com>
2024-06-17 22:02:54 +0900

    💯 test: ResultsのStory作成

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,8 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-styling-webpack',
+    'storybook-dark-mode',
   ],
   framework: {
     name: '@storybook/nextjs',
