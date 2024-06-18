ba4f4339cd2e29a54bd7fe7b18537457d4faa984
reckyy <srreki98@gmail.com>
2024-06-18 17:58:16 +0900

    ⚡️ feat: 本棚をルートに表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Login from '@/components/Login';
 import SearchBooksButton from '@/components/button/SearchBooksButton';
+import BookItems from '@/components/bookshelf/BookItems';
 
 export default function Home() {
   return (
     <div>
       <SearchBooksButton />
+      <BookItems />
       <Login />
     </div>
   );
---
+++ src/components/bookshelf/BookItems.tsx
@@ -0,0 +1,36 @@
+import BookItem from './BookItem';
+import { SimpleGrid } from '@mantine/core';
+import { auth } from '@/auth';
+
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+  cover_image_url: string;
+};
+
+const BookItems = async () => {
+  const session = await auth();
+  const res = await fetch(
+    `http://localhost:3001/api/books?email=${session?.user.email}`,
+  );
+  const bookItems = await res.json();
+
+  return (
+    <div>
+      {bookItems.length > 0 ? (
+        <SimpleGrid cols={3}>
+          {bookItems.map((book: Book) => (
+            <div key={book.id}>
+              <BookItem book={book} />
+            </div>
+          ))}
+        </SimpleGrid>
+      ) : (
+        <p>検索結果がありません。</p>
+      )}
+    </div>
+  );
+};
+
+export default BookItems;
