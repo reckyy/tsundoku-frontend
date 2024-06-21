025dad50d5e740fc78b9b594931e736f10ae896c
reckyy <srreki98@gmail.com>
2024-06-21 16:45:18 +0900

    🚀 style: ログインボタンをGoogle仕様に変更

---
+++ src/components/button/GoogleButton.tsx
@@ -0,0 +1,8 @@
+import { Button, ButtonProps } from '@mantine/core';
+import GoogleIcon from '../icon/GoogleIcon';
+
+const GoogleButton = (props: ButtonProps & React.ComponentPropsWithoutRef<'button'>) => {
+  return <Button leftSection={<GoogleIcon />} variant="default" {...props} />;
+}
+
+export default GoogleButton
