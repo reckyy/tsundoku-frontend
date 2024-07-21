7ed247bfc920a695be820b35a4675ca8a16868ee
reckyy <srreki98@gmail.com>
2024-07-21 12:23:03 +0900

    ⚡️ feat: ReadingLogをルートに追加
    現時点では、Railsから取得するのみ。

---
+++ src/components/Cal/Cal.tsx
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import CalContent from './CalContent';
+import { auth } from '@/auth';
+
+export default async function Cal() {
+  const session = await auth();
+  const params = { uid: session?.user?.id };
+  const res = await axios.get('http://localhost:3001/api/reading_logs', { params });
+  const readingLogs = res.data;
+
+  return <CalContent readingLogs={readingLogs} />;
+}
