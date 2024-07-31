bbbfcab14f28b1f641d3337ff6cf78d2165a1f19
reckyy <srreki98@gmail.com>
2024-07-31 15:06:21 +0900

    💯 test: prettier

--- src/components/search/SearchForm.tsx
+++ src/components/search/SearchForm.tsx
@@ -68,7 +68,7 @@ const SearchForm = ({ onResults }: SearchFormProps) => {
             <TextInput
               radius={'xl'}
               size="md"
-              label='検索'
+              label="検索"
               placeholder="本のタイトルや著者"
               rightSectionWidth={42}
               leftSection={
