233dc2a52d2b203814b435d1c5359d10b1cf5058
reckyy <srreki98@gmail.com>
2024-08-02 14:56:59 +0900

    📃 chore: 未検索時、検索バーのみ表示するように変更

--- src/components/search/Results.tsx
+++ src/components/search/Results.tsx
@@ -13,36 +13,34 @@ import { BookItemsProps } from '@/types/index';
 const Results = ({ bookItems }: BookItemsProps) => {
   return (
     <Grid justify="center" align="stretch">
-      {bookItems.length > 0 ? (
-        bookItems.map((book) => (
-          <GridCol span={6} key={book.id}>
-            <Card padding="md" withBorder shadow="sm" radius="md">
-              <SimpleGrid cols={{ base: 1, sm: 2 }}>
-                <Image
-                  radius="lg"
-                  w={141}
-                  h={200}
-                  fit="contain"
-                  src={book.coverImageUrl}
-                  alt={book.title}
-                />
-                <Flex
-                  gap="sm"
-                  justify="center"
-                  align="center"
-                  direction="column"
-                >
-                  <Text lineClamp={2}>{book.title}</Text>
-                  <Text lineClamp={1}>{book.author}</Text>
-                  <AddBookConfirmButton book={book} />
-                </Flex>
-              </SimpleGrid>
-            </Card>
-          </GridCol>
-        ))
-      ) : (
-        <p>検索結果がありません。</p>
-      )}
+      {bookItems.length > 0
+        ? bookItems.map((book) => (
+            <GridCol span={6} key={book.id}>
+              <Card padding="md" withBorder shadow="sm" radius="md">
+                <SimpleGrid cols={{ base: 1, sm: 2 }}>
+                  <Image
+                    radius="lg"
+                    w={141}
+                    h={200}
+                    fit="contain"
+                    src={book.coverImageUrl}
+                    alt={book.title}
+                  />
+                  <Flex
+                    gap="sm"
+                    justify="center"
+                    align="center"
+                    direction="column"
+                  >
+                    <Text lineClamp={2}>{book.title}</Text>
+                    <Text lineClamp={1}>{book.author}</Text>
+                    <AddBookConfirmButton book={book} />
+                  </Flex>
+                </SimpleGrid>
+              </Card>
+            </GridCol>
+          ))
+        : null}
     </Grid>
   );
 };
