418a9236ba3442c6b6b98960302cee5339ddd5e6
reckyy <srreki98@gmail.com>
2024-08-03 23:33:05 +0900

    🚀 style: FooterとHeaderの間隔を修正

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import { auth } from '#auth';
-import { Group, Text } from '@mantine/core';
+import { Group, Text, Container } from '@mantine/core';
 import Image from 'next/image';
 import UserMenu from './UserMenu';
 import Link from 'next/link';
@@ -8,26 +8,28 @@ export async function Header() {
   const session = await auth();
 
   return (
-    <Group h="100%" px="md">
-      <Group justify="space-between" style={{ flex: 1 }}>
-        <Link href={'/'}>
-          <Group gap="xs">
-            <Image src="/Tsundoku.png" alt="ロゴ" width={28} height={28} />
-            <Text fw={700} size="xl">
-              Tsundoku
-            </Text>
+    <Container size="md" p="md">
+      <Group h="100%" px="md">
+        <Group justify="space-between" style={{ flex: 1 }}>
+          <Link href={'/'}>
+            <Group gap="xs">
+              <Image src="/Tsundoku.png" alt="ロゴ" width={28} height={28} />
+              <Text fw={700} size="xl">
+                Tsundoku
+              </Text>
+            </Group>
+          </Link>
+          <Group ml="xl" gap={0}>
+            {session?.user && (
+              <UserMenu
+                name={session.user.name!}
+                image={session.user.image!}
+                id={session.user.id!}
+              />
+            )}
           </Group>
-        </Link>
-        <Group ml="xl" gap={0}>
-          {session?.user && (
-            <UserMenu
-              name={session.user.name!}
-              image={session.user.image!}
-              id={session.user.id!}
-            />
-          )}
         </Group>
       </Group>
-    </Group>
+    </Container>
   );
 }
