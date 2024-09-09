b41dee4c44374a4846398e41fbd0af6f08ddcca3
reckyy <srreki98@gmail.com>
2024-09-09 10:15:03 +0900

    💯 test: /loadingの単体テスト作成

---
+++ src/stories/loading/MemoLoading.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import MemoLoading from '@/components/loading/MemoLoading';
+
+
+const meta: Meta<typeof MemoLoading> = {
+  component: MemoLoading,
+  parameters: {
+    layout: 'centered',
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof MemoLoading>;
+
+export const AppearenceTest: Story = {
+  args: {},
+};
