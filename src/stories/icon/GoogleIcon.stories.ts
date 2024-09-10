4c93940db314afccc0184d961e84eece5aa75a4d
reckyy <srreki98@gmail.com>
2024-09-10 16:58:08 +0900

    💯 test: /iconの単体テスト作成

---
+++ src/stories/icon/GoogleIcon.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import GoogleIcon from '@/components/icon/GoogleIcon';
+
+const meta: Meta<typeof GoogleIcon> = {
+  component: GoogleIcon,
+  parameters: {
+    layout: 'centered',
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof GoogleIcon>;
+
+export const AppearenceTest: Story = {
+  args: {}
+};
