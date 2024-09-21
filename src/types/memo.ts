08312c20d3e7f4839c1a6396b1f07d8800000c1a
reckyy <srreki98@gmail.com>
2024-09-21 10:40:08 +0900

    🐛 fix: 画面のサイズによって、メモ画面のレイアウトを変更

--- src/types/memo.ts
+++ src/types/memo.ts
@@ -1,3 +1,4 @@
+import { Heading } from './heading';
 export type Memo = {
   id: number;
   body: string;
@@ -6,4 +7,15 @@ export type Memo = {
 export type MemoParams = {
   userId: string | undefined;
   bookId: number;
+};
+
+export type GridItemType = {
+  imageSpan: number | undefined;
+  headingSpan: number | undefined;
+  offset: number | undefined;
+  imageSrc: string | undefined;
+  imageAlt: string | undefined;
+  headings: Heading[];
+  heading: string;
+  setHeading: React.Dispatch<React.SetStateAction<string>>;
 };
