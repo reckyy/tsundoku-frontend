3f1c77ff96399a5c61a37d206042af1ca2eed2c9
reckyy <srreki98@gmail.com>
2024-09-23 16:40:16 +0900

    ⚡️ feat: Topページのスケルトンを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import BookItems from '@/components/bookshelf/BookItems';
 import Calendar from '@/components/calendar/Calendar';
 import { BookResponse } from '@/types/index';
 import type { Metadata } from 'next';
+import TopLoading from '@/components/loading/TopLoading';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Tsundoku',
@@ -13,7 +15,15 @@ export const metadata: Metadata = {
     'Tsundokuは読書が続かない人、積読しがちな人向けの読書管理アプリです。読んできた本の一覧表示と読書ログを公開することにより、外部に読書習慣をアピールできます！',
 };
 
-export default async function Home() {
+export default function Home() {
+  return (
+    <Suspense fallback={<TopLoading />}>
+      <HomeContent />
+    </Suspense>
+  );
+}
+
+async function HomeContent() {
   const session = await auth();
   if (session?.user) {
     const getBooks = async () => {
