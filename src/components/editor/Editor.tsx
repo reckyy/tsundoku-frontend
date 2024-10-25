42f8e40d3db74c71daa5edfe7217140d25b0cbec
reckyy <srreki98@gmail.com>
2024-10-25 16:45:16 +0900

    📃 chore: メモ画面のデザイン変更

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -11,7 +11,7 @@ import { Color } from '@tiptap/extension-color';
 import js from 'highlight.js/lib/languages/javascript';
 import ts from 'highlight.js/lib/languages/typescript';
 import rb from 'highlight.js/lib/languages/ruby';
-import { Button, Grid, GridCol, TextInput } from '@mantine/core';
+import { Button, Grid, GridCol, TextInput, Text } from '@mantine/core';
 import { useEffect, useRef, useState } from 'react';
 import { Heading } from '@/types/index';
 import classes from './Editor.module.css';
@@ -28,6 +28,18 @@ lowlight.register({ js, ts, rb });
 export default function Editor({ heading, handleSave }: EditorProps) {
   const memoBody = heading?.memo.body ?? '';
   const title = heading?.title ?? '';
+  const memoPlaceholder = `1. 1章 - プログラミングの基礎概念
+- プログラミングは、問題解決のための手順を記述する行為である。
+- アルゴリズムとデータ構造の基礎について学ぶ。
+
+2. 2章 - 変数とデータ型
+- 変数はデータを格納するための名前付きの場所であり、数値・文字列などのデータ型がある。
+- メモリの管理や効率的なデータ処理の重要性を理解。
+
+3. 3章 - 条件分岐とループ
+- 条件分岐（if文）やループ（for文、while文）によりプログラムの流れを制御。
+- 複雑なロジックを構築するための基礎を学ぶ。
+`;
 
   const editor = useEditor({
     extensions: [
@@ -37,7 +49,7 @@ export default function Editor({ heading, handleSave }: EditorProps) {
       TaskItem.configure({
         nested: true,
       }),
-      Placeholder.configure({ placeholder: 'メモを書く' }),
+      Placeholder.configure({ placeholder: memoPlaceholder }),
       Link.configure({
         openOnClick: true,
         autolink: true,
@@ -72,12 +84,13 @@ export default function Editor({ heading, handleSave }: EditorProps) {
     <>
       <Grid>
         <GridCol span={9}>
+          <Text fw="700">タイトル</Text>
           <TextInput
             variant="unstyled"
             aria-label="heading-title"
-            size="xl"
+            size="lg"
             value={headingTitle}
-            placeholder="章のタイトルを書く"
+            placeholder="第1章 : プログラミングの基礎概念"
             styles={{
               input: {
                 color: '#37352f',
@@ -86,14 +99,20 @@ export default function Editor({ heading, handleSave }: EditorProps) {
             onChange={(event) => setHeadingTitle(event.currentTarget.value)}
           />
         </GridCol>
-        <GridCol span={3}>
-          <Button variant="light" color="green" onClick={handleSaveClick}>
+        <GridCol offset={1} span={2}>
+          <Button
+            variant="light"
+            fullWidth
+            color="green"
+            onClick={handleSaveClick}
+          >
             保存
           </Button>
         </GridCol>
       </Grid>
       <Grid>
         <GridCol span={12}>
+          <Text fw="700">この章のメモ</Text>
           <RichTextEditor editor={editor} className={classes.customEditor}>
             <RichTextEditor.Content />
           </RichTextEditor>
