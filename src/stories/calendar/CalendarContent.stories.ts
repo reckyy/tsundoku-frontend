93c4b3051eb634ed24256720082ef4e31255d3dc
reckyy <srreki98@gmail.com>
2025-02-07 21:52:33 +0900

    🐛 fix: readingLogsが存在しない場合の条件を分岐
    まだ読書ログを作成していないuserの場合、エラーになってしまうため

--- src/stories/calendar/CalendarContent.stories.ts
+++ src/stories/calendar/CalendarContent.stories.ts
@@ -4,6 +4,7 @@ import CalendarContent from '@/components/calendar/CalendarContent';
 
 const previousDay = dayjs().subtract(1, 'd').format('YYYY-MM-DD');
 const today = dayjs().format('YYYY-MM-DD');
+const currentYear = String(dayjs().year());
 
 const meta: Meta<typeof CalendarContent> = {
   component: CalendarContent,
@@ -21,7 +22,7 @@ type Story = StoryObj<typeof CalendarContent>;
 export const AppearenceTest: Story = {
   args: {
     readingLogs: {
-      '2024': [
+      [currentYear]: [
         {
           date: previousDay,
           count: 1,
@@ -32,5 +33,11 @@ export const AppearenceTest: Story = {
         },
       ],
     },
+  },
+};
+
+export const NoLogTest: Story = {
+  args: {
+    readingLogs: {},
   },
 };
